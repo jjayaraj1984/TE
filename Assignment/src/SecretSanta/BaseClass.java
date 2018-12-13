6b3bd0b53a9eabf167bc44778af5681af4e05944
Jonathan Jayaraj <albin127@gmail.com>
2018-12-13 10:46:44 -0800

    New method

--- Assignment/src/SecretSanta/BaseClass.java
+++ Assignment/src/SecretSanta/BaseClass.java
@@ -1,10 +1,29 @@
 package SecretSanta;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		addParticipants();
 
+	}
+	
+	public static void addParticipants() {
+		System.out.println("Add participants. Type done when you're finished");
+		Scanner input = new Scanner(System.in);
+		String names = input.nextLine();
+		
+		ArrayList <String> participants = new ArrayList <String>();
+		
+		while(names != "done"){
+			
+			participants.add(names);
+				
+		}
+		
+		System.out.println(participants);
 	}
 
 }
